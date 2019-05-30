7dceed92f7ba4948e611545fe57377de72279c39
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-30 14:22:20 -0500

    send request to firebase function with base64

--- functions/index.js
+++ functions/index.js
@@ -2,32 +2,33 @@ const functions = require('firebase-functions');
 const vision = require('@google-cloud/vision');
 const client = new vision.ImageAnnotatorClient();
 const cors = require("cors")({origin: true});
+const fs = require("fs");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    console.log("RAN!!!")
-    response.send("Hello from Firebase!");
-  })
-});
 
 exports.imageLabelDetection = functions.https.onRequest(async (request, response) => {
   cors(request, response, async () => {
-    const image = 'https://i.redd.it/win02ukxgtb01.jpg';
+    // const image = 'https://i.redd.it/win02ukxgtb01.jpg';
+    
+    const body = JSON.parse(request.body);
 
-    const body = JSON.parse(request.body)
-    console.log(body);
+    fs.writeFileSync("/tmp/image.jpg", body.image, "base64", err => {
+      console.log(err);
+      return response.status(500).json({error: err});
+    });
+
+    const image = '/tmp/image.jpg';
 
     try {
-      // const labelDetection = await client.labelDetection(image);
+      const labelDetection = await client.labelDetection(image);
 
-      // return response.send(200, {
-      //   labelDetection: labelDetection[0]
-      // })
-      return response.send(200, 'OKAY');
+      return response.send(200, {
+        labelDetection: labelDetection[0]
+      })
+      // return response.send(200, 'OKAY');
     }
     catch (error) {
       return response.send(500, error);
