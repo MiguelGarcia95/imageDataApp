ed56bb5bc742df1c9a94ece941ead7150ad2d1fa
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-29 22:35:49 -0500

    redux connected

--- src/store/store.js
+++ src/store/store.js
@@ -8,7 +8,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
-  initialState
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 export default store;
