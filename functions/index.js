d28222eede458b2f5775c47628a88a00ca8394f1
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-30 08:47:39 -0500

    vision ai function works

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
-
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
 const cors = require("cors")({origin: true});
 
 // // Create and Deploy Your First Cloud Functions
@@ -14,5 +15,13 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 });
 
 exports.sendImageForVision = functions.https.onRequest((request, response) => {
-
+  cors(request, response, () => {
+    // const [result] = client.labelDetection('https://i.redd.it/win02ukxgtb01.jpg');
+    client.labelDetection('https://i.redd.it/win02ukxgtb01.jpg').then(res => {
+      // const labels = result.labelAnnotations;
+      // console.log(labels);
+      console.log(res[0]);
+      return response.send("IMAGE!");
+    }).catch(err => console.log(err));
+  })
 })
--- src/store/actions/test.js
+++ src/store/actions/test.js
@@ -2,8 +2,16 @@ import * as actionTypes from './actionTypes';
 
 export const testFunctions = () => {
   return dispatch => {
-    fetch(`https://us-central1-image-labeled-search.cloudfunctions.net/helloWorld`, {
+    fetch(` https://us-central1-image-labeled-search.cloudfunctions.net/sendImageForVision`, {
       method: 'POST'
     });
   }
 }
+
+// export const imageText = () => {
+//   return dispatch => {
+//     fetch(`https://us-central1-image-labeled-search.cloudfunctions.net/helloWorld`, {
+//       method: 'POST'
+//     });
+//   }
+// }
