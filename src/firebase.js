8b2151976559d469f2c8d55ea29cac09e3c4ba96
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-29 21:33:03 -0500

    connect to firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import {FIREBASE_API_KEY, FIREBASE_APP_ID} from './API_KEYS';
+
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: FIREBASE_API_KEY,
+  databaseURL: "https://image-labeled-search.firebaseio.com",
+  projectId: "image-labeled-search",
+  storageBucket: "",
+  messagingSenderId: "991120553633",
+  appId: FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
