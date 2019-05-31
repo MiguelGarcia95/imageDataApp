70db9c990b55512c34dd2ffed7b2aea86a8f678a
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-30 19:54:43 -0500

    create drag and drop component

--- src/components/UI/DragNDrop/index.jsx
+++ src/components/UI/DragNDrop/index.jsx
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
 
 class DragNDrop extends Component {
+  dropRef = React.createRef();
+
+  componentDidMount() {
+    let container = this.dropRef.current;
+    container.addEventListener('dragenter', this.handleDragIn);
+    container.addEventListener('dragleave', this.handleDragOut);
+    container.addEventListener('dragover', this.handleDrag);
+    container.addEventListener('drop', this.handleDrop);
+  }
+
   render() {
     return(
-      <section>
+      <section className='drag_drop_container' ref={this.dropRef}>
 
       </section>
     )
