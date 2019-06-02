804f2c9dad9bcf894be352aaee2622f3b27edcc2
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-02 00:30:52 -0500

    transform dragbox on filled

--- src/components/UI/Preview/index.jsx
+++ src/components/UI/Preview/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './style.css';
 
-const Preview = ({preview}) => {
+const Preview = ({preview, fullscreen}) => {
   return (
-    <section className="image_preview">
-      <section className="image">
-        {preview && <img src={preview} />}
+    <React.Fragment>
+      <section className="image_preview">
+        <section className="image">
+          {preview && <img src={preview} />}
+        </section>
       </section>
-    </section>
+
+      <section className={`image_fullscreen ${fullscreen ? 'open' : ''}`}>
+        <section className="exit"></section>
+        <section className="image_container">
+          <img src={preview} alt=""/>
+        </section>
+      </section>
+    </React.Fragment>
   )
 }
 
