55e088eb6ecd7272ba561a04819dc7493b704199
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-02 18:47:53 -0500

    connect actions to reducers

--- src/store/reducers/image.js
+++ src/store/reducers/image.js
@@ -1,10 +1,22 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
+  imageLabels: [],
+  imageWebLabels: []
 };
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
+    case actionTypes.SET_IMAGE_LABELS:
+      return {
+        ...state,
+        imageLabels: action.payload.imageLabels
+      }
+    case actionTypes.SET_IMAGE_WEB_LABELS:
+        return {
+          ...state,
+          imageWebLabels: action.payload.imageWebLabels
+        }
     default:
       return state;
   }
