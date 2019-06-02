12f039145d58c5353154e0cb4194b1b81781d6ac
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-02 02:11:28 -0500

    added icons

--- src/components/UI/Preview/index.jsx
+++ src/components/UI/Preview/index.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './style.css';
 
@@ -13,7 +15,9 @@ const Preview = ({preview, fullscreen, toggleOnImage, toggleOffImage}) => {
       </section>
 
       <section className={`image_fullscreen ${fullscreen ? 'open' : ''}`}>
-        <section className="exit"></section>
+        <section className="exit">
+          <FontAwesomeIcon onClick={toggleOffImage} fixedWidth  icon={faTimes} size="3x" color='white' />
+        </section>
         <section className="image_container">
           <img src={preview} alt="User Image"/>
         </section>
