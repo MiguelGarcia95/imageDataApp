525ee0d52750946bfcfea88af47ab51a38993154
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-03 12:33:57 -0500

    connect firebase functions

--- src/store/actions/image.js
+++ src/store/actions/image.js
@@ -230,8 +230,3 @@ export const setImageSafeSearch = safeSearch => {
     });
   }
 }
-
-
-//
-//
-//
--- src/store/reducers/image.js
+++ src/store/reducers/image.js
@@ -2,7 +2,11 @@ import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
   imageLabels: [],
-  imageWebLabels: []
+  imageWebLabels: [],
+  imageObjects: [],
+  imageText: [],
+  imageProperties: [],
+  imageSafeSearch: []
 };
 
 const reducer = (state = initialState, action) => {
@@ -13,10 +17,30 @@ const reducer = (state = initialState, action) => {
         imageLabels: action.payload.imageLabels.labelDetection.labelAnnotations
       }
     case actionTypes.SET_IMAGE_WEB_LABELS:
-        return {
-          ...state,
-          imageWebLabels: action.payload.imageWebLabels.webDetection.webDetection
-        }
+      return {
+        ...state,
+        imageWebLabels: action.payload.imageWebLabels.webDetection.webDetection
+      }
+    case actionTypes.SET_IMAGE_OBJECTS:
+      return {
+        ...state,
+        imageObjects: action.payload.imageObjects
+      }
+    case actionTypes.SET_IMAGE_TEXT:
+      return {
+        ...state,
+        imageText: action.payload.imageText
+      }
+    case actionTypes.SET_IMAGE_PROPERTIES:
+      return {
+        ...state,
+        imageProperties: action.payload.imageProperties
+      }
+    case actionTypes.SET_IMAGE_SAFE_SEARCH:
+      return {
+        ...state,
+        imageSafeSearch: action.payload.imageSafeSearch
+      }
     default:
       return state;
   }
