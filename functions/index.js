ecb33b7b4f6318674a360b1e22108e90cec8c164
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-02 23:43:00 -0500

    create firebase functions for extra api features

--- functions/index.js
+++ functions/index.js
@@ -29,7 +29,7 @@ exports.imageLabelDetection = functions.https.onRequest(async (request, response
     }
     catch (error) {
       console.log(error);
-      return response.send(500, error);
+      return response.status(500).json({error: err});
     }
   })
 })
@@ -53,24 +53,72 @@ exports.imageWebDetection = functions.https.onRequest(async (request, response) 
     }
     catch (error) {
       console.log(error);
-      return response.send(500, error);
+      return response.status(500).json({error: err});
     }
   })
 })
 
 
 exports.imageObjectDetection = functions.https.onRequest(async (request, response) => {
-  
+  cors(request, response, async () => {
+    const body = JSON.parse(request.body);
+    const incomingImage = Buffer(body.image, 'base64');
+    const imagePath = `/tmp/image.${body.type}`;
+
+    fs.writeFileSync(imagePath, incomingImage, err => {
+      console.log(err);
+      return response.status(500).json({error: err});
+    })
+
+    try {
+      const webDetection = await client.webDetection(imagePath);
+      return response.send(200, {
+        webDetection: webDetection[0]
+      })
+    }
+    catch (error) {
+      console.log(error);
+      return response.status(500).json({error: err});
+    }
+  })  
 })
 
 exports.imageTextDetection = functions.https.onRequest(async (request, response) => {
-  
+  cors(request, response, async () => {
+    const body = JSON.parse(request.body);
+    const incomingImage = Buffer(body.image, 'base64');
+    const imagePath = `/tmp/image.${body.type}`;
+
+    fs.writeFileSync(imagePath, incomingImage, err => {
+      console.log(err);
+      return response.status(500).json({error: err});
+    })
+  })
 })
 
 exports.imagePropertiesDetection = functions.https.onRequest(async (request, response) => {
-  
+  cors(request, response, async () => {
+    const body = JSON.parse(request.body);
+    const incomingImage = Buffer(body.image, 'base64');
+    const imagePath = `/tmp/image.${body.type}`;
+
+    fs.writeFileSync(imagePath, incomingImage, err => {
+      console.log(err);
+      return response.status(500).json({error: err});
+    })
+  })
 })
 
 exports.imageSafeSearchDetection = functions.https.onRequest(async (request, response) => {
-  
+  cors(request, response, async () => {
+    const body = JSON.parse(request.body);
+    const incomingImage = Buffer(body.image, 'base64');
+    const imagePath = `/tmp/image.${body.type}`;
+
+    fs.writeFileSync(imagePath, incomingImage, err => {
+      console.log(err);
+      return response.status(500).json({error: err});
+    })
+
+  })
 })
