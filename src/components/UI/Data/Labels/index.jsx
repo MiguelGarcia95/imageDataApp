4be6e1a0d344a6198b0287ee98b431ec7aee68b6
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-05 18:06:08 -0500

    display data

---
+++ src/components/UI/Data/Labels/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Labels({labels}) {
+  return (
+    <section>
+            
+    </section>
+  )
+}
+
+export default Labels
---
+++ src/components/UI/Data/WebLabels/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function WebLabels({labels}) {
+  return (
+    <section>
+            
+    </section>
+  )
+}
+
+export default WebLabels
+
