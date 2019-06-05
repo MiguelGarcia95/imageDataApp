f7bcba9da3cef2ecbd16ee41677ce72e519d541a
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-05 18:20:22 -0500

    display data

---
+++ src/components/UI/Data/SafeSearch/index.jsx
@@ -0,0 +1,13 @@
+// Unknown, Very Unlikely, Unlikely, Possible, Likely, and Very Likely
+ 
+import React from 'react';
+
+function SafeSearch({results}) {
+  return (
+    <section>
+            
+    </section>
+  )
+}
+
+export default SafeSearch
