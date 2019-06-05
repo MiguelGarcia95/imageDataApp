1c11b4d87e1573622dd3bffa38cdd791b1e70544
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-04 21:15:43 -0500

    style app

--- src/components/UI/MenuHeader/index.jsx
+++ src/components/UI/MenuHeader/index.jsx
@@ -4,7 +4,7 @@ import MenuItem from '../../UI/MenuItem';
 import {
   getImageLabels, getImageWebLabels, getImageObjects, getImageProperties, getImageSafeSearch, getImageText
 } from '../../../store/actions/image.js';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faTags, faChild, faObjectUngroup, faCogs, faWrench, faFont, faSearch } from '@fortawesome/free-solid-svg-icons';
 import './style.css';
 
 
@@ -25,18 +25,12 @@ class MenuHeader extends Component {
       <section className={`buttons opened ${preview ? 'opened' : '' }`}>
         {!preview && (
           <section className="button_container">
-            <MenuItem title='Web' icon={faTimes} onItemClick={() => this.onGetImageWebLabels(image)} />
-            {/* <MenuItem title='Web Detection' icon={faTimes} onItemClick={() => this.onGetImageWebLabels(image)} /> */}
-            <MenuItem title='Label' icon={faTimes} onItemClick={() => this.onGetImageLabels(image)}  />
-            {/* <MenuItem title='Label Detection' icon={faTimes} onItemClick={() => this.onGetImageLabels(image)}  /> */}
-            <MenuItem title='Objects' icon={faTimes} onItemClick={() => this.onGetImageObjects(image)}  />
-            {/* <MenuItem title='Object Detection' icon={faTimes} onItemClick={() => this.onGetImageObjects(image)}  /> */}
-            <MenuItem title='Properties' icon={faTimes} onItemClick={() => this.onGetImageProperties(image)}  />
-            {/* <MenuItem title='Image Properties' icon={faTimes} onItemClick={() => this.onGetImageProperties(image)}  /> */}
-            <MenuItem title='Safe Search' icon={faTimes} onItemClick={() => this.onGetImageSafeSearch(image)}  />
-            {/* <MenuItem title='Image Safe Search' icon={faTimes} onItemClick={() => this.onGetImageSafeSearch(image)}  /> */}
-            <MenuItem title='Text' icon={faTimes} onItemClick={() => this.onGetImageText(image)}  />
-            {/* <MenuItem title='Text Detection' icon={faTimes} onItemClick={() => this.onGetImageText(image)}  /> */}
+            <MenuItem title='Web' icon={faSearch} onItemClick={() => this.onGetImageWebLabels(image)} />
+            <MenuItem title='Label' icon={faTags} onItemClick={() => this.onGetImageLabels(image)}  />
+            <MenuItem title='Objects' icon={faObjectUngroup} onItemClick={() => this.onGetImageObjects(image)}  />
+            <MenuItem title='Properties' icon={faCogs} onItemClick={() => this.onGetImageProperties(image)}  />
+            <MenuItem title='Safe Search' icon={faChild} onItemClick={() => this.onGetImageSafeSearch(image)}  />
+            <MenuItem title='Text' icon={faFont} onItemClick={() => this.onGetImageText(image)}  />
           </section>
         ) }
       </section>
--- src/components/UI/MenuItem/index.jsx
+++ src/components/UI/MenuItem/index.jsx
@@ -5,7 +5,6 @@ const MenuItem = ({title, onItemClick, icon}) => {
   return (
     <section className="button" onClick={onItemClick}>
       <FontAwesomeIcon icon={icon} size='2x' color='white' className='icon' />
-      <small>Icon</small>
       <p>{title}</p>
     </section>
   )
