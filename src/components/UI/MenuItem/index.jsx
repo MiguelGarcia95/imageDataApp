43344dd2348870d61448fb26cabd62796e007a9a
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-04 22:46:51 -0500

    style app

--- src/components/UI/MenuItem/index.jsx
+++ src/components/UI/MenuItem/index.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const MenuItem = ({title, onItemClick, icon}) => {
   return (
     <section className="button" onClick={onItemClick}>
-      <FontAwesomeIcon icon={icon} size='2x' color='white' className='icon' />
+      <FontAwesomeIcon icon={icon} size='lg' color='white' className='icon' />
       <p>{title}</p>
     </section>
   )
