5c98ce1922796bdb2d574a0a0e488b0223d74587
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-05 18:07:03 -0500

    display data

---
+++ src/components/UI/Data/SImilarImages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SimilarImages({Images}) {
+  return (
+    <section>
+            
+    </section>
+  )
+}
+
+export default SimilarImages
---
+++ src/components/UI/Data/Text/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Text({text}) {
+  return (
+    <section>
+            
+    </section>
+  )
+}
+
+export default Text
