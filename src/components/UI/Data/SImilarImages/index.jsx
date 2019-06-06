b69ebd30503a4e587d79c6be93a9d6de1d0e460f
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-06 18:40:29 -0500

    show web data

--- src/components/UI/Data/SImilarImages/index.jsx
+++ src/components/UI/Data/SImilarImages/index.jsx
@@ -1,9 +1,55 @@
-import React from './node_modules/react';
+import React from 'react';
+
+displayMatchingImages = images => {
+  return images.fullMatchingImages.map((image, index) => {
+    return (
+      <section>
+
+      </section>
+    )
+  })
+}
+
+displayPartialMatchingImages = images => {
+  return images.partialMatchingImages.map((image, index) => {
+    return (
+      <section>
+
+      </section>
+    )
+  })
+}
+
+displayPagesWithMatchingImages = images => {
+  return images.pagesWithMatchingImages.map((image, index) => {
+    return (
+      <section>
+
+      </section>
+    )
+  })
+}
+
+displayVisuallySimilaryImages = images => {
+  return images.visuallySimilaryImages.map((image, index) => {
+    return (
+      <section>
+
+      </section>
+    )
+  })
+}
+
+// fullMatchingImages,
+// partialMatchingImages,
+// pagesWithMatchingImages,
+// visuallySimilaryImages,
+// bestGuessLabels
 
 function SimilarImages({images}) {
   return (
     <section>
-            
+
     </section>
   )
 }
