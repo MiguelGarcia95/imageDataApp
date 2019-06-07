72fd81d6248a3bccab44515e02b2c8a843b9f045
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-06 23:34:49 -0500

    show display images

--- src/components/UI/MenuItem/index.jsx
+++ src/components/UI/MenuItem/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const MenuItem = ({title, onItemClick, icon}) => {
-  return (
-    <section className="button" onClick={onItemClick}>
-      <FontAwesomeIcon icon={icon} size='lg' color='white' className='icon' />
-      <p>{title}</p>
-    </section>
-  )
+const MenuItem = ({title, onItemClick, icon, disabled}) => {
+  if (disabled) {
+    return (
+      <section className="button disabled">
+        <FontAwesomeIcon icon={icon} size='lg' color='white' className='icon' />
+        <p>{title}</p>
+      </section>
+    )
+  } else {
+    return (
+      <section className="button" onClick={onItemClick}>
+        <FontAwesomeIcon icon={icon} size='lg' color='white' className='icon' />
+        <p>{title}</p>
+      </section>
+    )
+  }
 };
 
 export default MenuItem;
