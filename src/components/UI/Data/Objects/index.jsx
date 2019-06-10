d45680cc0da5dbe2b238ee55f0d978a7bc19eb16
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-10 15:49:45 -0500

    display objects on grid

--- src/components/UI/Data/Objects/index.jsx
+++ src/components/UI/Data/Objects/index.jsx
@@ -12,27 +12,18 @@ const displasyObjects = objects => {
 }
 
 const verticies = (points, name) => {
-  // normalizedVertices[0] = top right origin,
-  // [0].x - [1].x = width 
-  // [1].y - [2].y = height 
-  // show this on .preview_grid 
   let gridBox = document.querySelector('.preview_grid');
   console.log(gridBox.clientHeight);
   console.log(gridBox.clientWidth);
   let width = Math.abs(points[0].x - points[1].x);
   let height = Math.abs(points[1].y - points[2].y);
   let origin = points[0];
-  // console.log('width', width);
-  // console.log('height', height);
-  // console.log('origin', origin); 
-  // x = gridWidth * origin.x
   let xOrigin = gridBox.clientWidth*origin.x;
   let yOrigin = gridBox.clientHeight*origin.y;
   let propHeight = gridBox.clientHeight*height;
   let propWidth = gridBox.clientWidth*width;
-  console.log('propHeight', propHeight);
-  console.log('propWidth', propWidth);
-  var grid = document.createElement('section');
+  let grid = document.createElement('section');
+  grid.title = name;
   grid.style.backgroundColor = 'transparent';
   grid.style.border = '1px solid red';
   grid.style.position = 'absolute';
@@ -42,10 +33,6 @@ const verticies = (points, name) => {
   grid.style.height = `${propHeight}px`;
   gridBox.appendChild(grid);
 }
-
-// boundingPoly.narmalizedVerticies - array
-// x y grid, starts at top left corner.
-//display it.
 
 function Objects({objects}) {
   return (
