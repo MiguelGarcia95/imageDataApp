03afea44cfa59507b2c392006d3bde519fd0328f
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-10 15:09:59 -0500

    display objects on grid

--- src/components/UI/Data/Objects/index.jsx
+++ src/components/UI/Data/Objects/index.jsx
@@ -17,16 +17,23 @@ const verticies = (points, name) => {
   // [1].y - [2].y = height 
   // show this on .preview_grid 
   let gridBox = document.querySelector('.preview_grid');
+  console.log(gridBox.clientHeight);
+  console.log(gridBox.clientWidth);
   let width = Math.abs(points[0].x - points[1].x);
   let height = Math.abs(points[1].y - points[2].y);
   let origin = points[0];
-  console.log('width', width);
-  console.log('height', height);
-  console.log('origin', origin);
-  // console.log(name)
-  // console.log(gridBox);
+  // console.log('width', width);
+  // console.log('height', height);
+  // console.log('origin', origin);
+  // GridBoxwidth/x = 1/width, x = GridBoxwidth/width 
+  // GridBoxheight/x = 1/height, x = GridBoxheight/height 
+  let propHeight = gridBox.clientHeight/height;
+  let propWidth = gridBox.clientWidth/width;
+  console.log('propHeight', propHeight);
+  console.log('propWidth', propWidth);
   var grid = document.createElement('section');
-  grid.style.backgroundColor = 'red';
+  grid.style.backgroundColor = 'transparent';
+  grid.style.border = '1px solid red';
   grid.style.width = '20px';
   grid.style.height = '20px';
   gridBox.appendChild(grid);
