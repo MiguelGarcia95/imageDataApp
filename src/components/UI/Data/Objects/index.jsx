4977a3f87984ab04fb38c413467a79d5a6e97a57
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-10 09:22:28 -0500

    display objects on grid

--- src/components/UI/Data/Objects/index.jsx
+++ src/components/UI/Data/Objects/index.jsx
@@ -4,11 +4,21 @@ const displasyObjects = objects => {
   if (!objects) return;
   return objects.map((object, index) => {
     return (
-      <section onClick={() => console.log(object.boundingPoly)} key={index} className="object">
+      <section onClick={() => verticies(object.boundingPoly, object.name)} key={index} className="object">
         <p>{object.name}</p>
       </section>
     )
   })
+}
+
+const verticies = (object, name) => {
+  // normalizedVertices[0] = top right origin,
+  // [0].x - [1].x = width 
+  // [1].y - [2].y = height 
+  // show this on .preview_grid 
+  console.log(object.normalizedVertices);
+  console.log(object);
+  console.log(name)
 }
 
 // boundingPoly.narmalizedVerticies - array
