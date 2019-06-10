c6d47682cf432a80dbabfdbf1de7f72b5c9bb19b
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-10 17:14:03 -0500

    display objects on grid

--- src/components/UI/Data/Objects/index.jsx
+++ src/components/UI/Data/Objects/index.jsx
@@ -6,7 +6,6 @@ const displasyObjects = objects => {
     setTimeout(() => {
       verticies(object.boundingPoly.normalizedVertices, object.name)
     }, 2000);
-    // <section onClick={() => verticies(object.boundingPoly.normalizedVertices, object.name)} key={index} className="object">
     return (
       <section key={index} className="object">
         <p>{object.name}</p>
@@ -20,19 +19,15 @@ const verticies = (points, name) => {
   let width = Math.abs(points[0].x - points[1].x);
   let height = Math.abs(points[1].y - points[2].y);
   let origin = points[0];
-  let xOrigin = gridBox.clientWidth*origin.x;
-  let yOrigin = gridBox.clientHeight*origin.y;
-  let propHeight = gridBox.clientHeight*height;
-  let propWidth = gridBox.clientWidth*width;
   let grid = document.createElement('section');
   grid.title = name;
   grid.style.backgroundColor = 'transparent';
   grid.style.border = '1px solid red';
   grid.style.position = 'absolute';
-  grid.style.top = `${yOrigin}px`;
-  grid.style.left = `${xOrigin}px`;
-  grid.style.width = `${propWidth}px`;
-  grid.style.height = `${propHeight}px`;
+  grid.style.top = `${gridBox.clientHeight*origin.y}px`;
+  grid.style.left = `${gridBox.clientWidth*origin.x}px`;
+  grid.style.width = `${gridBox.clientWidth*width}px`;
+  grid.style.height = `${gridBox.clientHeight*height}px`;
   gridBox.appendChild(grid);
 }
 
