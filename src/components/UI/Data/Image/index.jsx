cce29666cd0feb59ac279514fd3650f1e53d439a
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-10 22:39:24 -0500

    display objects on grid

--- src/components/UI/Data/Image/index.jsx
+++ src/components/UI/Data/Image/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import LazyLoad from 'react-lazy-load';
 
-function Image({image, index}) { 
+function Image({image}) { 
   return (
-    <LazyLoad offsetVertical={300} debounce={false} throttle={250} key={index}>
+    <LazyLoad offsetVertical={300} debounce={false} throttle={250} >
       <img className='similar_web_images' alt='similar image' src={image.url} />
     </LazyLoad>
   )
--- src/components/UI/Data/SImilarImages/index.jsx
+++ src/components/UI/Data/SImilarImages/index.jsx
@@ -1,55 +1,31 @@
 import React from 'react';
-import LazyLoad from 'react-lazy-load';
 import Image from '../Image';
 import './style.css';
 
-const imageError = image => {
-  console.log(image);
-
-}
+// const imageError = image => {
+//   console.log(image);
+// }
 
 const displayMatchingImages = images => {
   if (!images) return;
   return images.fullMatchingImages.map((image, index) => {
-    return (
-      // <LazyLoad offsetVertical={300} debounce={false} throttle={250} key={index}>
-      //   <img className='similar_web_images' alt='similar image' src={image.url} />
-      // </LazyLoad>
-      <Image index={index} image={image} />
-    )
+    return <Image key={index} image={image} />
   })
 }
 
 const displayPartialMatchingImages = images => {
   if (!images) return;
   return images.partialMatchingImages.map((image, index) => {
-    return (
-      // <LazyLoad offsetVertical={300} debounce={false} throttle={250} key={index}>
-      //    <img className='similar_web_images' alt='matching image' src={image.url} />
-      // </LazyLoad>
-      <Image index={index} image={image} />
-
-    ) 
+    return <Image key={index} image={image} />
   })
 }
 
 const displayVisuallySimilarImages = images => {
   if (!images) return;
   return images.visuallySimilarImages.map((image, index) => {
-    return (
-      // <LazyLoad offsetVertical={300} debounce={false} throttle={250} key={index} >
-      //    <img className='similar_web_images' alt='visually similar image' src={image.url} />
-      // </LazyLoad>
-      <Image index={index} image={image} />
-    )
+    return <Image key={index} image={image} />
   })
 }
-
-// fullMatchingImages,
-// partialMatchingImages,
-// pagesWithMatchingImages,
-// visuallySimilaryImages,
-// bestGuessLabels
 
 function SimilarImages({images}) { 
   return (
